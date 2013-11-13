8cc1f921fd9829170f9a5d2a677706c88b9b2a8b
goldem64 <goldem64.master@aol.com>
2013-11-12 21:40:16 -0600

    agregando cosas variable y comentario

--- analizador_sintactico_mysql/src/analizadorsintacticomysql/scanner.java
+++ analizador_sintactico_mysql/src/analizadorsintacticomysql/scanner.java
@@ -1,7 +1,11 @@
 package analizadorsintacticomysql;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
+//Autor : Gustavo Rosas Martinez
 public class scanner {
+
+private String[] token;
 
 }
  	 	
